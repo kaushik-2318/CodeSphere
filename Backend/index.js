4808e6fc2fc5c457a76f069c0fee75c25b555c0b
kaushik-2318 <kaushikverma321@gmail.com>
2024-11-09 02:12:05 +0530

    "Updated CORS origin from 'https://codesphere-seven.vercel.app/' to 'https://codesphere-seven.vercel.app'"

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "https://codesphere-seven.vercel.app/",
+  origin: "https://codesphere-seven.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
