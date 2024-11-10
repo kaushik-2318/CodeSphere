3130b8a50a84a55ba3368382af420a3484ad4f79
kaushik-2318 <kaushikverma321@gmail.com>
2024-11-11 01:17:23 +0530

    getScreenShot Updated

--- Backend/controllers/getScreenshot.controller.js
+++ Backend/controllers/getScreenshot.controller.js
@@ -1,8 +1,7 @@
-const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
-const path = require('path');
+const puppeteer = require('puppeteer');
+const path = require('path')
 const { v4: uuid } = require('uuid');
-const fs = require('fs');
+const fs = require('fs')
 
 const screenshot = async (req, res) => {
     const url = req.query.url;
@@ -13,48 +12,40 @@ const screenshot = async (req, res) => {
 
     let browser;
     try {
-        // Launch Puppeteer with serverless-compatible settings
+
         browser = await puppeteer.launch({
-            args: chromium.args,
-            executablePath: await chromium.executablePath || '/usr/bin/chromium-browser',
-            headless: chromium.headless,
             ignoreHTTPSErrors: true,
+            headless: 'true'
         });
 
         const page = await browser.newPage();
 
-        // Log the browser version for debugging
         await page.browser().version().then(function (version) {
-            console.log("Browser version:", version);
+            console.log(version);
         });
 
         try {
-            // Navigate to the specified URL and wait until the network is idle
             await page.goto(url, { waitUntil: 'networkidle2' });
         } catch (err) {
-            // Handle URL-specific errors
             if (err.message.includes('net::ERR_NAME_NOT_RESOLVED') || err.message.includes('net::ERR_SSL_UNRECOGNIZED_NAME_ALERT')) {
                 return res.status(400).json({ message: 'Invalid URL' });
-            } else {
-                return res.status(400).json({ message: 'Invalid URL' });
+            }
+            else {
+                return res.status(400).json({ message: 'Invalid URL' })
             }
         }
 
-        // Set viewport size
         await page.setViewport({ width: 1920, height: 1080 });
 
-        // Generate a unique filename and save the screenshot
         const fileName = `${uuid()}.png`;
         const screenshotPath = path.join(__dirname, '..', 'public', fileName);
         await page.screenshot({ path: screenshotPath });
 
-        // Return the path to the saved screenshot
         res.json({ screenshotPath: `/image/${fileName}` });
     } catch (err) {
         console.error('Error capturing screenshot:', err);
         res.status(500).json({ error: 'Failed to capture screenshot' });
     } finally {
-        // Ensure the browser is closed after completion or error
         if (browser) {
             await browser.close();
         }
